b691dc84ea4dd9a1818339dd50012a6e41598e5b
Felipe Andrade <feandrade.work@gmail.com>
2025-09-22 20:30:25 -0300

    arquivos criados referente a laços de repetição, scanner e outros atributos importantes em java.

---
+++ src/Condicional.java
@@ -0,0 +1,21 @@
+public class Condicional {
+    public static void main (String[] args){
+        int anoDeLancamento = 2022;
+        boolean incluidoPlano = false;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "ronald";
+
+
+        if (anoDeLancamento>= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        } else{
+            System.out.println("Filme Retrô que vale a pena assistir!");
+        }
+
+        if(incluidoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado!");
+        } else {
+            System.out.println("Mude a sua assinatura para a assinatura Plus para ter acesso a este filme.");
+        }
+    }
+}
---
+++ src/JogoAdivinhacao.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdivinhacao {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        int numeroAleatorio = new Random().nextInt(100);
+        int contador = 0;
+        int numeroChutado = 0;
+            while (numeroChutado != numeroAleatorio) {
+                System.out.println("Advinhe o número de 0 a 100 que o computador sorteou para nós.");
+                numeroChutado = leitura.nextInt();
+
+                if (numeroChutado > numeroAleatorio){
+                    System.out.println("Este número é maior que o número aleatório gerado, tente novamente.");
+                } else if (numeroChutado < numeroAleatorio) {
+                    System.out.println("Este número é menor que o número aleatório gerado, tente novamente");
+                } else {
+                    System.out.println("Parabéns, você acertou o número aleatório!");
+                }
+                break;
+            }
+    }
+}
